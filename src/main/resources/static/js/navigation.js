d20218421f294650f627124a4ec1a385d21945dd
liuyuanhai <554935169@qq.com>
2018-11-22 18:27:46 +0800

    完成了一小步

---
+++ src/main/resources/static/js/navigation.js
@@ -0,0 +1,14 @@
+$(function () {
+
+
+    //添加
+    $('.addNav').bind('click',function(){
+
+        //控制新增框数量
+        debugger;
+        var _html = '<li><input type="text" class="navInput" value=""/><span class="ybtn"></span><span class="nbtn"></span></li>';
+        $(this).parent().eq(0).before(_html);
+
+    });
+})
+
